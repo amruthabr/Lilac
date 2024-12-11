e2c1b237960dc8aa814ac0497075a63ab2559e84
amruthabr <33354940+amruthabr@users.noreply.github.com>
2024-12-11 14:31:56 +0530

    Update Designation.php

--- app/Models/Designation.php
+++ app/Models/Designation.php
@@ -6,8 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Designation extends Model
 {
-
-    // Define relationship with User
     public function users()
     {
         return $this->hasMany(User::class, 'fk_designation');
