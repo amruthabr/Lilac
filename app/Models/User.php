a09c9b9f89070e6c465ebd2c277e8f4ea1182072
amruthabr <33354940+amruthabr@users.noreply.github.com>
2024-12-11 14:32:16 +0530

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,13 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
 
-    // Define relationship with Designation
     public function designation()
     {
         return $this->belongsTo(Designation::class, 'fk_designation');
     }
 
-    // Define relationship with Department
     public function department()
     {
         return $this->belongsTo(Department::class, 'fk_department');
